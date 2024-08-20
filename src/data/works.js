c2cf022332fbbe5e657c09c0e25e1a77a8e934c7
Nelyda Yeray Medina Martin del campo <beccca.becbec@gmail.com>
2024-08-20 15:46:37 -0600

    Traeme unas carnitas yarvis~

---
+++ src/data/works.js
@@ -0,0 +1,22 @@
+export const works = [
+    {
+      id: "library-assistant",
+      image: "/img/library_assistant.png",
+      name: "Library Assistant",
+      description:
+        "Sistema para el Control de Asistencia de la Biblioteca dentro de la UTZAC.",
+      url: "",
+      author: "Nelyda Yeray Mc",
+      year: 2024,
+    },
+    {
+        id: "library-assistant",
+        image: "/img/library_assistant.png",
+        name: "library-assistant",
+        description:
+          "Sistema para el Control de Asistencia de la Biblioteca dentro de la UTZAC.",
+        url: "",
+        author: "Nelyda Yeray Mc",
+        year: 2024,
+      }
+];
