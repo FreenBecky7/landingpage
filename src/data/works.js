4c51161442d9dfe7a6bd9297b5cee46beab88b17
Nelyda Yeray Medina Martin del campo <beccca.becbec@gmail.com>
2025-03-06 14:37:54 -0600

    Aiiii si weeeee

--- src/data/works.js
+++ src/data/works.js
@@ -11,13 +11,33 @@ export const works = [
       
     },
     {
-        id: "library-assistant",
-        image: "/img/library_assistant.png",
-        name: "library-assistant",
+        id: "App de Cafeteria",
+        image: "/img/Cfe.png",
+        name: "LaCafe",
         description:
-          "Sistema para el Control de Asistencia de la Biblioteca dentro de la UTZAC.",
+          "Plataforma digital para gestionar pedidos de comida en LaCafe de manera rápida y eficiente.",
         url: "",
         author: "Nelyda Yeray Mc",
         year: 2024,
-      }
+      },
+      {
+        id: "Brain Trap",
+        image: "/img/Btrap.png",
+        name: "Brain Trap",
+        description:
+          "Juego para PC donde controlas a un zombi que recolecta cerebros mientras esquiva a los enemigos. ¡Un solo toque y pierdes!.",
+        url: "",
+        author: "Nelyda Yeray Mc",
+        year: 2024,
+      },
+      {
+        id: "Homework list",
+        image: "/img/list.png",
+        name: "Homework list",
+        description:
+          "Plataforma web para organizar tus tareas de forma eficiente, con sincronización en la nube para acceder a tu información desde cualquier lugar.",
+        url: "",
+        author: "Nelyda Yeray Mc",
+        year: 2024,
+      },
 ];
